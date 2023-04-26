29a38c34d8d397de4dd636dd0889e3af090d00dd
Jasur977 <jasurmamatqulov.uz@gmail.com>
2023-04-27 01:25:32 +0500

    update changes

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,14 @@ Route::get('/contacts', [\App\Http\Controllers\ContactController::class, 'index'
 
 Route::post('/contacts', [\App\Http\Controllers\ContactController::class, 'store'] )->name('contacts.store');
 
+
+
 Route::get('/contacts/create', [\App\Http\Controllers\ContactController::class, 'create'])->name('contacts.create');
 
 Route::get('/contacts/{id}', [\App\Http\Controllers\ContactController::class, 'show'])->name('contacts.show');
+
+Route::put('/contacts/{contact}', [\App\Http\Controllers\ContactController::class, 'update'])->name('contacts.update');
+
+Route::get('/contacts/{contact}/edit', [\App\Http\Controllers\ContactController::class, 'edit'])->name('contacts.edit');
+
+//Route::get('/contacts/{id}/edit',[\App\Http\Controllers\ContactController::class, 'edit'])->name('contacts.edit');
